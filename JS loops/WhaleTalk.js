1c386432593cd2dd8cbe12661cbe66d2c6efee57
Gabrielius Vaitkus <gabrieliusvaitkus.gv@gmail.com>
2023-05-07 16:38:52 +0100

    Done

---
+++ JS loops/WhaleTalk.js
@@ -0,0 +1,20 @@
+let input = "Hi, Human";
+const vowels = ["a", "e", "i", "o", "u"];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "e") {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === "u") {
+    resultArray.push(input[i]);
+  }
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    }
+  }
+}
+
+const results = resultArray.join('').toUpperCase();
+console.log(results);

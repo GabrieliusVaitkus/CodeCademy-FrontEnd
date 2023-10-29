e4a72e194b197857f391dea9cde8b5d897697bad
Gabrielius Vaitkus <gabrieliusvaitkus.gv@gmail.com>
2023-10-29 13:51:10 +0000

    Finished the SImple-to-list

--- Exercise projecs/Simple To-Do List/script.js
+++ Exercise projecs/Simple To-Do List/script.js
@@ -4,30 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
     const addTaskButton = document.getElementById('addTask');
     const taskList = document.getElementById('taskList');
 
-    //Event listener for the button
+    //Event listener for the "Add task" button
     addTaskButton.addEventListener('click', function() {
-        //Value from the input field
         const taskValue = taskInput.value.trim();
-        //Check if input is not empty
         if (taskValue) {
-            //Creating new list item
             const listItem = document.createElement('li');
             listItem.textContent = taskValue;
-            //Append the new list item to the list
+            listItem.className = "taskItem"; // Assign a class for styling and event handling
+
+            // Add click event listener to the list item
+            listItem.addEventListener('click', function() {
+                this.classList.toggle('completed');
+            });
+
             taskList.appendChild(listItem);
-            //Clear the input field
             taskInput.value = "";
         } else {
             alert('Please enter a task');
-        }
-    })
-
-    //Event listtener to the task list for event delegation
-    taskList.addEventListener('click', function(event) {
-        //Check if the element is a list item
-        if (event.target.tagName === 'li') {
-            //Toggle completed on the list item
-            event.target.classList.toggle('completed');
         }
     })
 })
